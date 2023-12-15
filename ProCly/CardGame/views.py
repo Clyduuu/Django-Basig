4b8fb81b65c60f8b61dc1852e371564ac6dbb84d
Clyduuu <202080146@psu.palawan.edu.ph>
2023-12-15 15:06:20 +0800

    Done Changes

--- ProCly/CardGame/views.py
+++ ProCly/CardGame/views.py
@@ -1,41 +1,126 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views.generic.list import ListView
-from CardGame.models import PokemonCard,Trainer,Collection
+from CardGame.models import PokemonCard, Trainer, Collection
+from .form import TrainerForm, CollectionForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+import json
 
+# Create your views here.
 class HomePageView(ListView):
     model = PokemonCard
     context_object_name = 'home'
-    template_name = "base.html"
-    
+    template_name = 'home.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['segment'] = 'home'
         return context
-
-class TrainerList(ListView):
-    model = Trainer
-    context_object_name = 'trainer'
-    template_name = "trainer.html"
     
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        return context
-
-class PokemonCardList(ListView):
+class PokemonCardListView(ListView):
     model = PokemonCard
     context_object_name = 'pokemoncard'
-    template_name = "pokemon-card.html"
+    template_name = "pokemoncards.html"
+    json_file_path = 'data/pokemon_data.json'
+    paginate_by = 6
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        # context['segment'] = 'pokemon-card'
+        pokemon_data = self.get_pokemon_data()
+        context['pokemon_data'] = pokemon_data
         return context
     
+    def get_pokemon_data(self):
+        with open(self.json_file_path, 'r') as json_file:
+            pokemon_data = json.load(json_file)
+    
+        return pokemon_data
+    
+    
+    
+class TrainerListView(ListView):
+    model = Trainer
+    context_object_name = 'trainer'
+    template_name = 'trainers.html'
+    paginate_by = 5
+    ordering = ['-create_at'] 
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['segment'] = 'trainer-list'
+        return context
+    
+    
+
 class CollectionListView(ListView):
     model = Collection
     context_object_name = 'collection'
-    template_name = "collection.html"
-    
+    template_name = 'collection.html'
+    paginate_by = 3
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        context['segment'] = 'collection-list'
         return context
+    
 
-# Create your views here.
+class TrainerCreateView(CreateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_add.html'
+    success_url = reverse_lazy('trainer-list')
+
+
+class TrainerUpdateView(UpdateView):
+    model = Trainer
+    form_class = TrainerForm
+    template_name = 'trainer_edit.html'
+    success_url = reverse_lazy('trainer-list')
+
+
+class TrainerDeleteView(DeleteView):
+    model = Trainer
+    template_name = 'trainer_del.html'
+    success_url = reverse_lazy('trainer-list')
+
+
+class CollectionCreateView(CreateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'collection_add.html'
+    success_url = reverse_lazy('collection-list')
+    
+
+class CollectionUpdateView(UpdateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'collection_edit.html'
+    success_url = reverse_lazy('collection-list')
+
+
+class CollectionDeleteView(DeleteView):
+    model = Collection
+    template_name = 'collection_del.html'
+    success_url = reverse_lazy('collection-list')
+
+class PokemonCreateView(CreateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'pokemon_add.html'
+    success_url = reverse_lazy('pokemoncard-list')
+    paginate = 3
+
+class PokemonUpdateView(UpdateView):
+    model = Collection
+    form_class = CollectionForm
+    template_name = 'pokemon_edit.html'
+    success_url = reverse_lazy('pokemoncard-list')
+
+
+class PokemonDeleteView(DeleteView):
+    model = Collection
+    template_name = 'pokemon_del.html'
+    success_url = reverse_lazy('pokemoncard-list')
--- ProCly/ProCly/urls.py
+++ ProCly/ProCly/urls.py
@@ -1,8 +1,8 @@
 """
-URL configuration for ProCly project.
+URL configuration for projectsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -16,15 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from CardGame.views import HomePageView, TrainerList, PokemonCardList, CollectionListView
-from django.conf import settings
-from django.conf.urls.static import static
+from CardGame.views import HomePageView, TrainerListView, TrainerCreateView, TrainerUpdateView, TrainerDeleteView, CollectionListView, CollectionCreateView, CollectionUpdateView, CollectionDeleteView, PokemonCardListView, PokemonCreateView, PokemonUpdateView, PokemonDeleteView
+
+from CardGame import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomePageView.as_view(), name='home'),
-    path('trainers/', TrainerList.as_view(), name='trainer-list'),
-    path('pokemon-cards/', PokemonCardList.as_view(), name='pokemon-cards'),
-    path('collection/', CollectionListView.as_view(), name='collection'),
-    # Add other paths for your views as needed
+    path('', views.HomePageView.as_view(), name='home'),
+    path('trainer_list', TrainerListView.as_view(), name='trainer-list'),
+    path('trainer_list/add', TrainerCreateView.as_view(), name='trainer-add'),
+    path('trainer_list/<pk>', TrainerUpdateView.as_view(), name='trainer-update'),
+    path('trainer_list/<pk>/delete',TrainerDeleteView.as_view(), name='trainer-delete'),
+    path('collection_list', CollectionListView.as_view(), name='collection-list'),
+    path('collection_list/add', CollectionCreateView.as_view(), name='collection-add'),
+    path('collection_list/<pk>', CollectionUpdateView.as_view(), name='collection-update'),
+    path('collection_list/<pk>/delete',CollectionDeleteView.as_view(), name='collection-delete'),
+    path('pokemoncard_list', PokemonCardListView.as_view(), name='pokemoncard-list'),
+    path('pokemoncard_list/add', PokemonCreateView.as_view(), name='pokemon_add'),
+    path('pokemoncard_list/<pk>', PokemonUpdateView.as_view(), name='pokemon_edit'),
+    path('pokemoncard_list/<pk>/delete',PokemonDeleteView.as_view(), name='pokemon_del'),
+    
+
 ]
